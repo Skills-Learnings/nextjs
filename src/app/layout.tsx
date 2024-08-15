12cd55766690eec0db7175c4a45c77c7e4858aca
SahilAxel <sahil.gidwani@axelerant.com>
2024-08-15 15:11:36 +0530

    Routing basics code example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,31 @@
+import Link from "next/link"
+
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/team">Team</Link>
+          </li>
+          <li>
+            <Link href="/team/about">Team About</Link>
+          </li>
+        </ul>
+      </nav>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
