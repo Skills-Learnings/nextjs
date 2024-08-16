20c723457c644fc5c4befd4a974f1736093a6050
SahilAxel <sahil.gidwani@axelerant.com>
2024-08-16 15:43:14 +0530

    Loading and errors code example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import { Suspense } from "react"
+
+async function getTodos() {
+  await wait(2000)
+  return fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+    res.json()
+  )
+}
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Todos</h1>
+      <Suspense fallback={"Loading..."}>
+        <TodoList />
+      </Suspense>
+    </>
+  )
+}
+
+async function TodoList() {
+  const todos = await getTodos()
+
+  return <p>{todos.length}</p>
+}
+function wait(duration: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration)
+  })
 }
