53861af2b92d40d699741ad0e6b71b4dc12ba977
SahilAxel <sahil.gidwani@axelerant.com>
2024-09-02 13:52:36 +0530

    Pages directory app metadata examples

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import Head from "next/head"
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <Head>
+        <title>This is the title</title>
+        <meta property="description" content="Desc 1" key="DEsc"/>
+      </Head>
+      <h1>Home</h1>
+      <NestedComponent/>
+    </>
+  )
+}
+
+function NestedComponent() {
+  return (
+    <>
+      <Head>
+        <title>This is the title 2</title>
+        <meta property="description" content="Desc 2" key="DEsc"/>
+      </Head>
+    </>
+  )
 }
