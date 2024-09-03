92a7ca639710cd67cd562293e9f461efc6d34f96
SahilAxel <sahil.gidwani@axelerant.com>
2024-09-03 15:21:50 +0530

    Scripts code examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import { roboto } from "@/fonts"
-import Image from "next/image"
-import MountainImage from "../imgs/mountain.jpg"
+"use client"
 
 export default function Home() {
-  /* Example of rendering local image  */
-  /* return (
-    <div style={{ width: "500px", height: "500px", position: "relative" }}>
-      <Image placeholder="blur" fill src={MountainImage} alt="Image" />
-    </div>
-  ) */
-
-  /* Example of rendering remote image  */
   return (
-    <Image width={500} height={500} alt="remote image" src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    <h1 onClick={() => {
+      $("h1").text("Bye")
+    }}>Hi</h1>
   )
 }
