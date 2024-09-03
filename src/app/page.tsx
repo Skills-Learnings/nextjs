c3bd24b4e5425f8a141a51deff997ca32799daf2
SahilAxel <sahil.gidwani@axelerant.com>
2024-09-03 12:36:01 +0530

    Fonts examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { roboto } from "@/fonts";
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Test</h1>
+      <p className={roboto.className}>This is the font</p>
+    </>
+  )
 }
